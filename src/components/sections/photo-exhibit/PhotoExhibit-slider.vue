087648b9c57c1daf80062a9d6e135c9457714be3
Julius Ian Valdez <julius2614@gmail.com>
2024-11-20 13:36:03 +0800

    feat(animations): :dizzy:

---
+++ src/components/sections/photo-exhibit/PhotoExhibit-slider.vue
@@ -0,0 +1,360 @@
+<template>
+  <h1>Photo Exhibit</h1>
+
+  <main class="exhibit">
+    <div class="exhibit__grid grid-2" style="--quantity: 5">
+      <figure class="exhibit__grid-item exhibit__grid-item--1" style="--position: 1">
+        <img
+          src="@/assets/images/exhibit/l-1.webp"
+          class="exhibit__grid-image exhibit__grid-image--1"
+          alt=""
+        />
+        <div class="exhibit__grid-item-detail">
+          <p>Elem Days</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__grid-item exhibit__grid-item--2" style="--position: 2">
+        <img
+          src="@/assets/images/exhibit/p-1.webp"
+          class="exhibit__grid-image exhibit__grid-image--2"
+          alt=""
+        />
+        <div class="exhibit__grid-item-detail">
+          <p>Mini Venice</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__grid-item exhibit__grid-item--3" style="--position: 3">
+        <img
+          src="@/assets/images/exhibit/l-2.webp"
+          class="exhibit__grid-image exhibit__grid-image--3"
+          alt=""
+        />
+        <div class="exhibit__grid-item-detail">
+          <p>Water Bender</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__grid-item exhibit__grid-item--4" style="--position: 4">
+        <img
+          src="@/assets/images/exhibit/p-2.webp"
+          class="exhibit__grid-image exhibit__grid-image--4"
+          alt=""
+        />
+        <div class="exhibit__grid-item-detail">
+          <p>Vast Sea</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__grid-item exhibit__grid-item--5" style="--position: 5">
+        <img
+          src="@/assets/images/exhibit/l-6.webp"
+          class="exhibit__grid-image exhibit__grid-image--5"
+          alt=""
+        />
+        <div class="exhibit__grid-item-detail">
+          <p>Food Riders</p>
+        </div>
+      </figure>
+
+      <!-- <figure class="exhibit__grid-item exhibit__grid-item--6">
+        <img
+          src="@/assets/images/exhibit/p-6.webp"
+          class="exhibit__grid-image exhibit__grid-image--6"
+          alt=""
+        />
+        <div class="exhibit__grid-item-detail">
+          <p>Grand Bell</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__grid-item exhibit__grid-item--7">
+        <img
+          src="@/assets/images/exhibit/l-4.webp"
+          class="exhibit__grid-image exhibit__grid-image--7"
+          alt=""
+        />
+        <div class="exhibit__grid-item-detail">
+          <p>Our Freedom</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__grid-item exhibit__grid-item--8">
+        <img
+          src="@/assets/images/exhibit/l-5.webp"
+          class="exhibit__grid-image exhibit__grid-image--8"
+          alt=""
+        />
+        <div class="exhibit__grid-item-detail">
+          <p>Filipino Street Foods</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__grid-item exhibit__grid-item--9">
+        <img
+          src="@/assets/images/exhibit/p-4.webp"
+          class="exhibit__grid-image exhibit__grid-image--9"
+          alt=""
+        />
+        <div class="exhibit__grid-item-detail">
+          <p>Neon Lights</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__grid-item exhibit__grid-item--10">
+        <img
+          src="@/assets/images/exhibit/l-3.webp"
+          class="exhibit__grid-image exhibit__grid-image--10"
+          alt=""
+        />
+        <div class="exhibit__grid-item-detail">
+          <p>Paint the Town White</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__grid-item exhibit__grid-item--11">
+        <img
+          src="@/assets/images/exhibit/l-7.webp"
+          class="exhibit__grid-image exhibit__grid-image--11"
+          alt=""
+        />
+        <div class="exhibit__grid-item-detail">
+          <p>X-Mas Season</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__grid-item exhibit__grid-item--12">
+        <img
+          src="@/assets/images/exhibit/p-3.webp"
+          class="exhibit__grid-image exhibit__grid-image--12"
+          alt=""
+        />
+        <div class="exhibit__grid-item-detail">
+          <p>My Bestfriend</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__grid-item exhibit__grid-item--13">
+        <img
+          src="@/assets/images/exhibit/p-5.webp"
+          class="exhibit__grid-image exhibit__grid-image--13"
+          alt=""
+        />
+        <div class="exhibit__grid-item-detail">
+          <p>Follow the Star</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__grid-item exhibit__grid-item--14">
+        <img
+          src="@/assets/images/exhibit/l-8.webp"
+          class="exhibit__grid-image exhibit__grid-image--14"
+          alt=""
+        />
+        <div class="exhibit__grid-item-detail">
+          <p>Perya</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__grid-item exhibit__grid-item--15">
+        <img
+          src="@/assets/images/exhibit/l-9.webp"
+          class="exhibit__grid-image exhibit__grid-image--15"
+          alt=""
+        />
+        <div class="exhibit__grid-item-detail">
+          <p>Shall We Play</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__grid-item exhibit__grid-item--16">
+        <img
+          src="@/assets/images/exhibit/l-10.webp"
+          class="exhibit__grid-image exhibit__grid-image--16"
+          alt=""
+        />
+        <div class="exhibit__grid-item-detail">
+          <p>Lock Love</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__grid-item exhibit__grid-item--17">
+        <img
+          src="@/assets/images/exhibit/l-11.webp"
+          class="exhibit__grid-image exhibit__grid-image--17"
+          alt=""
+        />
+        <div class="exhibit__grid-item-detail">
+          <p>The Sampaguitta</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__grid-item exhibit__grid-item--18">
+        <img
+          src="@/assets/images/exhibit/l-12.webp"
+          class="exhibit__grid-image exhibit__grid-image--18"
+          alt=""
+        />
+        <div class="exhibit__grid-item-detail">
+          <p>Young scavengers</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__grid-item exhibit__grid-item--19">
+        <img
+          src="@/assets/images/exhibit/l-13.webp"
+          class="exhibit__grid-image exhibit__grid-image--19"
+          alt=""
+        />
+        <div class="exhibit__grid-item-detail">
+          <p>Flawless beam</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__grid-item exhibit__grid-item--20">
+        <img
+          src="@/assets/images/exhibit/l-19.webp"
+          class="exhibit__grid-image exhibit__grid-image--20"
+          alt=""
+        />
+        <div class="exhibit__grid-item-detail">
+          <p>Pommelhorse future star</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__grid-item exhibit__grid-item--21">
+        <img
+          src="@/assets/images/exhibit/l-15.webp"
+          class="exhibit__grid-image exhibit__grid-image--21"
+          alt=""
+        />
+        <div class="exhibit__grid-item-detail">
+          <p>Morning gossip</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__grid-item exhibit__grid-item--22">
+        <img
+          src="@/assets/images/exhibit/l-16.webp"
+          class="exhibit__grid-image exhibit__grid-image--22"
+          alt=""
+        />
+        <div class="exhibit__grid-item-detail">
+          <p>Passing by</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__grid-item exhibit__grid-item--23">
+        <img
+          src="@/assets/images/exhibit/l-17.webp"
+          class="exhibit__grid-image exhibit__grid-image--23"
+          alt=""
+        />
+        <div class="exhibit__grid-item-detail">
+          <p>Grill it</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__grid-item exhibit__grid-item--24">
+        <img
+          src="@/assets/images/exhibit/l-18.webp"
+          class="exhibit__grid-image exhibit__grid-image--24"
+          alt=""
+        />
+        <div class="exhibit__grid-item-detail">
+          <p>Laundrove</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__grid-item exhibit__grid-item--25">
+        <img
+          src="@/assets/images/exhibit/p-7.webp"
+          class="exhibit__grid-image exhibit__grid-image--25"
+          alt=""
+        />
+        <div class="exhibit__grid-item-detail">
+          <p>Merry merry</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__grid-item exhibit__grid-item--26">
+        <img
+          src="@/assets/images/exhibit/l-14.webp"
+          class="exhibit__grid-image exhibit__grid-image--26"
+          alt=""
+        />
+        <div class="exhibit__grid-item-detail">
+          <p>Young star</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__grid-item exhibit__grid-item--27">
+        <img
+          src="@/assets/images/exhibit/l-20.webp"
+          class="exhibit__grid-image exhibit__grid-image--27"
+          alt=""
+        />
+        <div class="exhibit__grid-item-detail">
+          <p>By the thread</p>
+        </div>
+      </figure> -->
+    </div>
+  </main>
+
+  <div></div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+@import '@/assets/main.scss';
+
+h1 {
+  text-align: center;
+  text-transform: uppercase;
+  font-family: $font-poppins;
+  font-size: $font-size-large;
+}
+
+.exhibit {
+  width: 100%;
+  height: 12rem;
+  overflow: hidden;
+
+  &__grid {
+    margin-top: 0.8rem;
+    display: flex;
+    width: 100%;
+    min-width: calc(100px * var(--quantity));
+    position: relative;
+    animation: autoRunLeft 20s linear infinite;
+    left: 100%;
+  }
+
+  &__grid-item {
+    width: 10rem;
+    height: 10rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    animation-delay: calc((20s / var(--quantity)) * (var(--position) - 1) - 20s) !important;
+    transition: transform 0.4s ease-in-out;
+    cursor: pointer;
+
+    &:hover {
+      filter: grayscale(0);
+      transform: scale(1.05);
+    }
+  }
+
+  &__grid-image {
+    width: 100%;
+  }
+
+  &__grid-item-detail {
+    visibility: hidden;
+  }
+}
+</style>
---
+++ src/components/sections/photo-exhibit/PhotoExhibit.vue
@@ -0,0 +1,324 @@
+<template>
+  <h1>Photo Exhibit</h1>
+
+  <main class="exhibit">
+    <div class="exhibit__wheel">
+      <figure class="exhibit__wheel-card exhibit__wheel-card--1" style="--position: 1">
+        <img
+          src="@/assets/images/exhibit/l-1.webp"
+          class="exhibit__grid-image exhibit__grid-image--1"
+          alt=""
+        />
+        <div class="exhibit__wheel-card-detail">
+          <p>Elem Days</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__wheel-card exhibit__wheel-card--2" style="--position: 2">
+        <img
+          src="@/assets/images/exhibit/p-1.webp"
+          class="exhibit__grid-image exhibit__grid-image--2"
+          alt=""
+        />
+        <div class="exhibit__wheel-card-detail">
+          <p>Mini Venice</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__wheel-card exhibit__wheel-card--3" style="--position: 3">
+        <img
+          src="@/assets/images/exhibit/l-2.webp"
+          class="exhibit__grid-image exhibit__grid-image--3"
+          alt=""
+        />
+        <div class="exhibit__wheel-card-detail">
+          <p>Water Bender</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__wheel-card exhibit__wheel-card--4" style="--position: 4">
+        <img
+          src="@/assets/images/exhibit/p-2.webp"
+          class="exhibit__grid-image exhibit__grid-image--4"
+          alt=""
+        />
+        <div class="exhibit__wheel-card-detail">
+          <p>Vast Sea</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__wheel-card exhibit__wheel-card--5" style="--position: 5">
+        <img
+          src="@/assets/images/exhibit/l-6.webp"
+          class="exhibit__grid-image exhibit__grid-image--5"
+          alt=""
+        />
+        <div class="exhibit__wheel-card-detail">
+          <p>Food Riders</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__wheel-card exhibit__wheel-card--6">
+        <img
+          src="@/assets/images/exhibit/p-6.webp"
+          class="exhibit__grid-image exhibit__grid-image--6"
+          alt=""
+        />
+        <div class="exhibit__wheel-card-detail">
+          <p>Grand Bell</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__wheel-card exhibit__wheel-card--7">
+        <img
+          src="@/assets/images/exhibit/l-4.webp"
+          class="exhibit__grid-image exhibit__grid-image--7"
+          alt=""
+        />
+        <div class="exhibit__wheel-card-detail">
+          <p>Our Freedom</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__wheel-card exhibit__wheel-card--8">
+        <img
+          src="@/assets/images/exhibit/l-5.webp"
+          class="exhibit__grid-image exhibit__grid-image--8"
+          alt=""
+        />
+        <div class="exhibit__wheel-card-detail">
+          <p>Filipino Street Foods</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__wheel-card exhibit__wheel-card--9">
+        <img
+          src="@/assets/images/exhibit/p-4.webp"
+          class="exhibit__grid-image exhibit__grid-image--9"
+          alt=""
+        />
+        <div class="exhibit__wheel-card-detail">
+          <p>Neon Lights</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__wheel-card exhibit__wheel-card--10">
+        <img
+          src="@/assets/images/exhibit/l-3.webp"
+          class="exhibit__grid-image exhibit__grid-image--10"
+          alt=""
+        />
+        <div class="exhibit__wheel-card-detail">
+          <p>Paint the Town White</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__wheel-card exhibit__wheel-card--11">
+        <img
+          src="@/assets/images/exhibit/l-7.webp"
+          class="exhibit__grid-image exhibit__grid-image--11"
+          alt=""
+        />
+        <div class="exhibit__wheel-card-detail">
+          <p>X-Mas Season</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__wheel-card exhibit__wheel-card--12">
+        <img
+          src="@/assets/images/exhibit/p-3.webp"
+          class="exhibit__grid-image exhibit__grid-image--12"
+          alt=""
+        />
+        <div class="exhibit__wheel-card-detail">
+          <p>My Bestfriend</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__wheel-card exhibit__wheel-card--13">
+        <img
+          src="@/assets/images/exhibit/p-5.webp"
+          class="exhibit__grid-image exhibit__grid-image--13"
+          alt=""
+        />
+        <div class="exhibit__wheel-card-detail">
+          <p>Follow the Star</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__wheel-card exhibit__wheel-card--14">
+        <img
+          src="@/assets/images/exhibit/l-8.webp"
+          class="exhibit__grid-image exhibit__grid-image--14"
+          alt=""
+        />
+        <div class="exhibit__wheel-card-detail">
+          <p>Perya</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__wheel-card exhibit__wheel-card--15">
+        <img
+          src="@/assets/images/exhibit/l-9.webp"
+          class="exhibit__grid-image exhibit__grid-image--15"
+          alt=""
+        />
+        <div class="exhibit__wheel-card-detail">
+          <p>Shall We Play</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__wheel-card exhibit__wheel-card--16">
+        <img
+          src="@/assets/images/exhibit/l-10.webp"
+          class="exhibit__grid-image exhibit__grid-image--16"
+          alt=""
+        />
+        <div class="exhibit__wheel-card-detail">
+          <p>Lock Love</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__wheel-card exhibit__wheel-card--17">
+        <img
+          src="@/assets/images/exhibit/l-11.webp"
+          class="exhibit__grid-image exhibit__grid-image--17"
+          alt=""
+        />
+        <div class="exhibit__wheel-card-detail">
+          <p>The Sampaguitta</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__wheel-card exhibit__wheel-card--18">
+        <img
+          src="@/assets/images/exhibit/l-12.webp"
+          class="exhibit__grid-image exhibit__grid-image--18"
+          alt=""
+        />
+        <div class="exhibit__wheel-card-detail">
+          <p>Young scavengers</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__wheel-card exhibit__wheel-card--19">
+        <img
+          src="@/assets/images/exhibit/l-13.webp"
+          class="exhibit__grid-image exhibit__grid-image--19"
+          alt=""
+        />
+        <div class="exhibit__wheel-card-detail">
+          <p>Flawless beam</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__wheel-card exhibit__wheel-card--20">
+        <img
+          src="@/assets/images/exhibit/l-19.webp"
+          class="exhibit__grid-image exhibit__grid-image--20"
+          alt=""
+        />
+        <div class="exhibit__wheel-card-detail">
+          <p>Pommelhorse future star</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__wheel-card exhibit__wheel-card--21">
+        <img
+          src="@/assets/images/exhibit/l-15.webp"
+          class="exhibit__grid-image exhibit__grid-image--21"
+          alt=""
+        />
+        <div class="exhibit__wheel-card-detail">
+          <p>Morning gossip</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__wheel-card exhibit__wheel-card--22">
+        <img
+          src="@/assets/images/exhibit/l-16.webp"
+          class="exhibit__grid-image exhibit__grid-image--22"
+          alt=""
+        />
+        <div class="exhibit__wheel-card-detail">
+          <p>Passing by</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__wheel-card exhibit__wheel-card--23">
+        <img
+          src="@/assets/images/exhibit/l-17.webp"
+          class="exhibit__grid-image exhibit__grid-image--23"
+          alt=""
+        />
+        <div class="exhibit__wheel-card-detail">
+          <p>Grill it</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__wheel-card exhibit__wheel-card--24">
+        <img
+          src="@/assets/images/exhibit/l-18.webp"
+          class="exhibit__grid-image exhibit__grid-image--24"
+          alt=""
+        />
+        <div class="exhibit__wheel-card-detail">
+          <p>Laundrove</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__wheel-card exhibit__wheel-card--25">
+        <img
+          src="@/assets/images/exhibit/p-7.webp"
+          class="exhibit__grid-image exhibit__grid-image--25"
+          alt=""
+        />
+        <div class="exhibit__wheel-card-detail">
+          <p>Merry merry</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__wheel-card exhibit__wheel-card--26">
+        <img
+          src="@/assets/images/exhibit/l-14.webp"
+          class="exhibit__grid-image exhibit__grid-image--26"
+          alt=""
+        />
+        <div class="exhibit__wheel-card-detail">
+          <p>Young star</p>
+        </div>
+      </figure>
+
+      <figure class="exhibit__wheel-card exhibit__wheel-card--27">
+        <img
+          src="@/assets/images/exhibit/l-20.webp"
+          class="exhibit__grid-image exhibit__grid-image--27"
+          alt=""
+        />
+        <div class="exhibit__wheel-card-detail">
+          <p>By the thread</p>
+        </div>
+      </figure>
+    </div>
+  </main>
+
+  <div class="scroll-down">Scroll down</div>
+  <div class="scroll-down-icon">
+    <p>&DownArrow;</p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+@import '@/assets/main.scss';
+
+h1 {
+  text-align: center;
+  text-transform: uppercase;
+  font-family: $font-poppins;
+  font-size: $font-size-large;
+}
+</style>
+
+<script setup></script>
