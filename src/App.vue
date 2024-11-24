3b20e8139f209e7ba4097baa3e2e28028b8dfb45
Julius Ian Valdez <julius2614@gmail.com>
2024-11-24 18:34:42 +0800

    fix: :rewind: preloader missing fix

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <script setup>
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
-import PreLoader from './components/preloader/PreLoader.vue';
+  import { ref } from 'vue';
+  import { useRouter } from 'vue-router';
+  import PreLoader from './components/preloader/PreLoader.vue';
 
-const isLoading = ref(false);
-const router = useRouter();
+  const isLoading = ref(false);
+  const router = useRouter();
 
-router.beforeEach((to, from, next) => {
-  isLoading.value = true;
-  next();
-});
+  router.beforeEach((to, from, next) => {
+    isLoading.value = true;
+    next();
+  });
 
-router.afterEach(() => {
-  setTimeout(() => {
-    isLoading.value = false;
-  }, 2000);
-});
+  router.afterEach(() => {
+    setTimeout(() => {
+      isLoading.value = false;
+    }, 2000);
+  });
 </script>
 
 <template>
@@ -26,5 +26,5 @@ router.afterEach(() => {
 </template>
 
 <style lang="scss">
-@import '@/assets/main.scss';
+  @import '@/assets/main.scss';
 </style>
